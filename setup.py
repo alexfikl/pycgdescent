d9190249c75959d68ef559ee6689f142dad872b3
Alexandru Fikl <alexfikl@gmail.com>
2020-12-21 19:35:09 -0600

    add initial setup scripts

---
+++ setup.py
@@ -0,0 +1,16 @@
+import sys
+from setuptools import setup
+
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+sources = [
+        "src/cg_descent.c",
+        "src/cg_descent_wrap.cpp"
+        ]
+
+setup(
+        ext_modules=[
+            Pybind11Extension("cg_descent", sources),
+            ],
+        cmd_class={"build_ext": build_ext},
+        )
