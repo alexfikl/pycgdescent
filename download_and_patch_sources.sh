f05f2a2cc17e0d95e1b93e87bffcbce26b811aa8
Alexandru Fikl <alexfikl@gmail.com>
2023-02-12 07:41:12 +0200

    scripts: run shfmt

--- download_and_patch_sources.sh
+++ download_and_patch_sources.sh
@@ -27,8 +27,8 @@ pushd "${builddir}"
 echo -e "\033[1;32mbuilddir: $(pwd)\033[0m"
 
 if [ ! -f "${builddir}/${archive}" ]; then
-    echo -e "\033[1;32mDownloading...\033[0m"
-    curl -L -O "${url}"
+  echo -e "\033[1;32mDownloading...\033[0m"
+  curl -L -O "${url}"
 fi
 
 echo -e "\033[1;32mExtracting '${archive}'...\033[0m"
@@ -39,22 +39,21 @@ tar xvf "${archive}"
 # {{{ apply patches
 
 declare -a patches=(
-    '0000-add-blas-compile-flag.patch'
-    '0001-add-extern-c.patch'
-    '0002-add-header-guards.patch'
-    '0003-add-func-typedefs.patch'
-    '0004-add-user-pointer-to-functions.patch'
-    '0005-add-iteration-callback.patch'
-    '0006-add-step-size-limit.patch'
-    '0007-cg_evaluate-initialize-df.patch'
+  '0000-add-blas-compile-flag.patch'
+  '0001-add-extern-c.patch'
+  '0002-add-header-guards.patch'
+  '0003-add-func-typedefs.patch'
+  '0004-add-user-pointer-to-functions.patch'
+  '0005-add-iteration-callback.patch'
+  '0006-add-step-size-limit.patch'
+  '0007-cg_evaluate-initialize-df.patch'
 )
 
 pushd "${pkgname}-${pkgver}"
 pwd
-for patch in "${patches[@]}"
-do
-    echo -e "\033[1;32mApplying '${patchdir}/${patch}\033[0m'"
-    patch -p1 -i "${patchdir}/${patch}"
+for patch in "${patches[@]}"; do
+  echo -e "\033[1;32mApplying '${patchdir}/${patch}\033[0m'"
+  patch -p1 -i "${patchdir}/${patch}"
 done
 
 # }}}
@@ -62,9 +61,8 @@ done
 # {{{ copy sources
 
 echo -e "\033[1;32mCopying patched sources...\033[0m"
-for filename in cg_user.h cg_blas.h cg_descent.h cg_descent.c
-do
-    cp "${filename}" "${basedir}/src"
+for filename in cg_user.h cg_blas.h cg_descent.h cg_descent.c; do
+  cp "${filename}" "${basedir}/src"
 done
 
 popd
@@ -72,3 +70,5 @@ popd
 # rm -rf ${builddir}
 
 # }}}
+
+# vim:set ts=2 sts=2 sw=2 et:
